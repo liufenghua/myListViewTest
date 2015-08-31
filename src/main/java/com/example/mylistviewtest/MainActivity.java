a20e6dd4538f6bd0462c56037955ab62c7a11d49
liufenghua <liufenghua@vivo.com.cn>
2015-08-31 11:35:11 +0800

    再次修改

--- src/main/java/com/example/mylistviewtest/MainActivity.java
+++ src/main/java/com/example/mylistviewtest/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity implements OnClickListener{
         button3=(Button)this.findViewById(R.id.button3);
         button4=(Button)this.findViewById(R.id.button4);
         button5=(Button)this.findViewById(R.id.button5);
-        //最后合并的
+        //最后合并的是吗
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
         //我再次添加的注释
