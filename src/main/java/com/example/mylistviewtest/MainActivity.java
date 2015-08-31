cf5bea126dc417a7a6a804883aaa0e73314d4d40
liufenghua <liufenghua@vivo.com.cn>
2015-08-31 11:07:06 +0800

    第二次修改

--- src/main/java/com/example/mylistviewtest/MainActivity.java
+++ src/main/java/com/example/mylistviewtest/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity implements OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //xinzengjiadezhushi
         button1=(Button)this.findViewById(R.id.button1);
         button2=(Button)this.findViewById(R.id.button2);
         button3=(Button)this.findViewById(R.id.button3);
